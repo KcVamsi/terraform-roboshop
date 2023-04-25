6ff8214328814eeefc2da1f11018d43dab76a84f
kcvamsi <kcvamsi98@gmail.com>
2023-04-25 18:07:46 +0530

    tf

--- servers.tf
+++ servers.tf
@@ -3,7 +3,7 @@ owners           = ["973714476881"]
 most_recent      = true
 name_regex       = "Centos-8-DevOps-Practice"
 
-output "aws_ami" {
+output "ami" {
     value = data.aws_ami.centos.image_id
 }
 
