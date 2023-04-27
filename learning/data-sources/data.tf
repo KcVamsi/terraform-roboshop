9e05d770c26284c91c73ff244ec0302db6f90445
kcvamsi <kcvamsi98@gmail.com>
2023-04-27 18:40:31 +0530

    tf

--- learning/data-sources/data.tf
+++ learning/data-sources/data.tf
@@ -4,7 +4,7 @@ data "aws_security_group" "selected" {
 
 # as we know the name of the sg we are going with name otherwise we can go with the id also, but what we need here is id.
 
-output = "security_group_id" {
+output  "security_group_id" {
     value = data.aws_security_group.selected.id
 }
 
