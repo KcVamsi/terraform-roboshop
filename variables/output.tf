8ef69d8d3ec3326bc14c15ffcbb664f909f6e772
kcvamsi <kcvamsi98@gmail.com>
2023-05-03 13:00:53 +0530

    tf

--- variables/output.tf
+++ variables/output.tf
@@ -1,54 +1,54 @@
-# here we have a string to print
+# # here we have a string to print
 
-output "sample_string" {
-    value = var.sample_string
+# output "sample_string" {
+#     value = var.sample_string
   
-}
+# }
 
-# here we have a variable with a combination of some string to print
+# # here we have a variable with a combination of some string to print
 
-output "sample_string1" {
-    value = "value of sample_string = ${var.sample_string}"
+# output "sample_string1" {
+#     value = "value of sample_string = ${var.sample_string}"
   
-}
+# }
 
-# print the 2nd value in the lsit
-
-
-output "sample_list_2" {
-    value = var.sample_list[1]
-}
+# # print the 2nd value in the lsit
 
 
-# print number1 in the dict 
+# output "sample_list_2" {
+#     value = var.sample_list[1]
+# }
 
-output "sample_dict_number" {
-    value = var.sample_dict["number1"]
+
+# # print number1 in the dict 
+
+# output "sample_dict_number" {
+#     value = var.sample_dict["number1"]
   
-}
+# }
 
 
-output "sample_dict_boolean" {
-    value = var.sample_dict["boolean"]
+# output "sample_dict_boolean" {
+#     value = var.sample_dict["boolean"]
   
-}
+# }
 
 
 
-# for dynamically passing the values
+# # for dynamically passing the values
 
-output "env" {
-    value = var.env
+# output "env" {
+#     value = var.env
   
-}
+# }
 
 
-output "auto_num1" {
-    value = var.auto_num1
+# output "auto_num1" {
+#     value = var.auto_num1
   
-}
+# }
 
-output "sample1" {
-    value = var.sample1
+# output "sample1" {
+#     value = var.sample1
   
-}
+# }
