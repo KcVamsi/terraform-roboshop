2820a7220dcdd7365165415d94e6e5127d9b9c41
kcvamsi <kcvamsi98@gmail.com>
2023-05-04 17:05:14 +0530

    instabces

--- servers.tf
+++ servers.tf
@@ -39,220 +39,238 @@ variable "instance_type" {
 
 
 
-resource "aws_instance" "frontend" {
+# 4 # creating the instances for roboshop 
+# resource "aws_instance" "frontend" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "frontend"
+#   }
+# }
+
+
+# resource "aws_route53_record" "frontend" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "frontend.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.frontend.private_ip]
+# }
+
+# # here we use private_ip as we are connecting to internal servers.
+
+
+# resource "aws_instance" "mongodb" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "mongodb"
+#   }
+# }
+
+# resource "aws_route53_record" "mongodb" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "mongodb.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mongodb.private_ip]
+# }
+
+
+
+# resource "aws_instance" "catalogue" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "catalogue"
+#   }
+# }
+
+# resource "aws_route53_record" "catalogue" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "catalogue.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.catalogue.private_ip]
+# }
+
+
+
+# resource "aws_instance" "redis" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "reddis"
+#   }
+# }
+
+# resource "aws_route53_record" "redis" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "redis.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.redis.private_ip]
+# }
+
+
+# resource "aws_instance" "user" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "user"
+#   }
+# }
+
+# resource "aws_route53_record" "user" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "user.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.user.private_ip]
+# }
+
+
+
+# resource "aws_instance" "cart" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "cart"
+#   }
+# }
+
+# resource "aws_route53_record" "cart" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "cart.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.cart.private_ip]
+# }
+
+
+# resource "aws_instance" "mysql" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "mysql"
+#   }
+# }
+
+# resource "aws_route53_record" "mysql" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "mysql.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.mysql.private_ip]
+# }
+
+
+# resource "aws_instance" "shipping" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "shipping"
+#   }
+# }
+
+# resource "aws_route53_record" "shipping" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "shipping.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.shipping.private_ip]
+# }
+
+
+# resource "aws_instance" "rabbitmq" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "rabbitmq"
+#   }
+# }
+
+# resource "aws_route53_record" "rabbitmq" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "rabbitmq.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.rabbitmq.private_ip]
+# }
+
+
+# resource "aws_instance" "payment" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "payment"
+#   }
+# }
+
+# resource "aws_route53_record" "payment" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "payment.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.payment.private_ip]
+# }
+
+
+# resource "aws_instance" "dispatch" {
+#   ami           = data.aws_ami.centos.image_id
+#   instance_type = var.instance_type
+#   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
+
+#   tags = {
+#     Name = "dispatch"
+#   }
+# }
+
+# resource "aws_route53_record" "dispatch" {
+#   zone_id = "Z01467732RXIXT52PMHO"
+#   name    = "dispatch.devopsdomain1.online"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.dispatch.private_ip]
+# }
+
+
+
+# 5 #   instead creating one by one we use variable for the components and create all at a time
+#  after 1,2 and 3 we use this.
+
+
+variable "components" {
+  default = [ "frontend", "mongodb", "catalogue" ]
+}
+
+resource "aws_instance" "instance" {
+  count         = length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
 
   tags = {
-    Name = "frontend"
+    Name = "components"
   }
 }
-
-
-resource "aws_route53_record" "frontend" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "frontend.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.frontend.private_ip]
-}
-
-# here we use private_ip as we are connecting to internal servers.
-
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-resource "aws_route53_record" "mongodb" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "mongodb.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mongodb.private_ip]
-}
-
-
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "catalogue"
-  }
-}
-
-resource "aws_route53_record" "catalogue" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "catalogue.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.catalogue.private_ip]
-}
-
-
-
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "reddis"
-  }
-}
-
-resource "aws_route53_record" "redis" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "redis.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.redis.private_ip]
-}
-
-
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "user"
-  }
-}
-
-resource "aws_route53_record" "user" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "user.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.user.private_ip]
-}
-
-
-
-resource "aws_instance" "cart" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "cart"
-  }
-}
-
-resource "aws_route53_record" "cart" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "cart.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.cart.private_ip]
-}
-
-
-resource "aws_instance" "mysql" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "mysql"
-  }
-}
-
-resource "aws_route53_record" "mysql" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "mysql.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.mysql.private_ip]
-}
-
-
-resource "aws_instance" "shipping" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_route53_record" "shipping" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "shipping.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.shipping.private_ip]
-}
-
-
-resource "aws_instance" "rabbitmq" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
-
-resource "aws_route53_record" "rabbitmq" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "rabbitmq.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.rabbitmq.private_ip]
-}
-
-
-resource "aws_instance" "payment" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "payment"
-  }
-}
-
-resource "aws_route53_record" "payment" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "payment.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.payment.private_ip]
-}
-
-
-resource "aws_instance" "dispatch" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id]
-
-  tags = {
-    Name = "dispatch"
-  }
-}
-
-resource "aws_route53_record" "dispatch" {
-  zone_id = "Z01467732RXIXT52PMHO"
-  name    = "dispatch.devopsdomain1.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.dispatch.private_ip]
-}
-
-
-
-# 4 # 
