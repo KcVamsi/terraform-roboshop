fcfee4d1a89fd959307473c44e9e17888b3d3118
kcvamsi <kcvamsi98@gmail.com>
2023-05-04 16:38:47 +0530

    va;ues of fruits

--- loops/count/loops.tf
+++ loops/count/loops.tf
@@ -1,14 +1,14 @@
-#just create a null resource and see the output
+# 1 # just create a null resource and see the output
 
 resource "null_resource" "null" {
     count = 10
 }
 
 
-
 # if we wanto to create 10 servers then we can use this.
 
-# if we want to know the length of the function then we use 
+
+#2 # if we want to know the length of the function then we use 
 
 
 resource "null_resource" "fruits" {
@@ -17,21 +17,30 @@ resource "null_resource" "fruits" {
 }
 
 
+ 
+# 3 # if we want to deal with some inputs then we use the variables
+
+# variable "fruits" {
+#     default = [ "apple", "banana", "orange" ]
+  
+# }
+
+
+
+# 4 # if want the value of apple ,banan etc then we use provisioners.
 # we use provisioners after the resources and we can use at any place after the resource creation.
 
-#  provisioner "local-exec" {
-#   command = "echo fruit name - ${var.fruits[count.index]}"
 
-# }
+ provisioner "local-exec" {
+  command = "echo fruit name - ${var.fruits[count.index]}"
+ }
 
-# }
 
- 
-# if we want to deal with some inputs then we use the variables
+# here we use "echo ${}" because we have combination of variables and string.
+# [count.index] is used to pick the value one by one.
+# so after the resource instead of variables we use the provisoners and then variables
 
 variable "fruits" {
     default = [ "apple", "banana", "orange" ]
   
 }
-
-# we use provisioners after the resources and we can use at any place after the resource creation.
