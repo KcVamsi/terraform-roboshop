d169c0a03185ac53115c0c9ab967145781ff7ace
kcvamsi <kcvamsi98@gmail.com>
2023-05-04 16:24:19 +0530

    sg creation

--- data-sources/data.tf
+++ data-sources/data.tf
@@ -1,3 +1,5 @@
+# # as we know the name of the sg we are going with name otherwise we can go with the id also, but what we need here is id.
+
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 }
@@ -9,24 +11,4 @@ output "aws_security_group" {
 }
 
 
-
-# # as we know the name of the sg we are going with name otherwise we can go with the id also, but what we need here is id.
-
-# output "security_group_id" {
-#     value = data.aws_security_group.allow-all.id
-# }
-
-
-# # this is used if we want all the security groups
-
-#  data "aws_security_groups" "test"{
-
-#  }
-
-# output "all_Security_groups" {
-#     value = data.aws_security_groups.test
-  
-# }
-
-# # but here we are getting all the ids of the sg's but not all the information about the groups.
 
