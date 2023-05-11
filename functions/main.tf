439fb06eac6053cbb66d24a7786f213338e17660
kcvamsi <kcvamsi98@gmail.com>
2023-05-11 22:39:09 +0530

    funcctions

--- functions/main.tf
+++ functions/main.tf
@@ -6,6 +6,23 @@ variable "sample" {
 }
 
 output "sample" {
-    value = upper{var.sample}
+    value = upper(var.sample)
   
 }
+
+
+variable "sample1" {
+    default = [ "abc", "sads" ]
+  
+}
+
+# if we want to print the 1st value of the two then we can get it by function named "element"
+# or by the cmnd var.sample1[0]
+output "sample1" {
+    value = element(var.sample1, 0)
+  
+}
+
+
+
+
