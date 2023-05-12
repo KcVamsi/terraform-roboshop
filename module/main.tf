787b221250c79e9abf817931b64da3f59bb3b3ff
kcvamsi <kcvamsi98@gmail.com>
2023-05-12 12:44:10 +0530

    inputs for modules

--- module/main.tf
+++ module/main.tf
@@ -1,8 +1,21 @@
-# this block is used to call all the resources given in the other modules main.tf file
+# this is module block and is used to call all the resources given in the other modules main.tf file
 # we can call n number of resources at a single time using the module
+
+
+# module "sample" {
+#     source = "./module"
+  
+# }
+
+
+# if we want to give inputs to the module 
 
 
 module "sample" {
     source = "./module"
+    input = "assd"
   
 }
+
+# this inlut shuld be recieved by the module by using a variable module
+
--- module/module/main.tf
+++ module/module/main.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "nothing" {
     provisioner "local-exec" {
-    command = "echo hello world from module"
+    command = "echo hello world from module - input - ${var.input}"
     }
   
 }
@@ -8,4 +8,13 @@ resource "null_resource" "nothing" {
 # local-exec is used
 
 # here we are decalring the variables
+
+
+
+# Inputs to the Module can be called by variable block
+
+variable "input" {
+  
+}
+
 
